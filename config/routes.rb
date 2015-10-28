3c88b9a28e5444568a9697186fb2f7900606e9f9
Shain Lafazan <srslafazan@gmail.com>
2015-10-28 10:59:25 -0700

    cleaned up sign-up form, organized routes.rb, resolved image-load errors

--- config/routes.rb
+++ config/routes.rb
@@ -2,25 +2,28 @@ Rails.application.routes.draw do
 
   root 'welcomes#index'
 
+  resources :products
+  resources :sessions
+  resources :subscriptions
+  resources :users
+ 
+  # NAV ROUTING
+  get 'about' => 'welcomes#about'
   get 'welcomes/about' => 'welcomes#about'
 
-  post 'sessions/new' => "sessions#new"
-
+  # SESSIONS
   post 'session' => "sessions#create"
-
+  post 'sessions/new' => "sessions#new"
   get 'sessions/destroy' => "sessions#destroy"
 
+  # DASHBOARD
   get 'dashboard' => "dashboards#index"
   
-  resources 'users'
+  # ADMIN
+  get 'products/admin' => 'products#admin'
 
-  get "products/admin" => "products#admin"
-
-  resources 'products'
-
-  resources 'subscriptions'
-
-
+  # OAUTH LOGIN
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
+
 end
